d3beba8608ea2b1f1953aa189349a8f7a8c33f71
Igor Merkulov <avprevolucion@gmail.com>
2020-05-21 19:18:34 +0300

    Create populate.sql

---
+++ populate.sql
@@ -0,0 +1,70 @@
+Insert into HumanSex(sex)
+Values('F');
+
+Insert into HumanSex(sex)
+Values('M');
+
+
+Insert into HumanHealth(health)
+Values(1);
+
+Insert into HumanHealth(health)
+Values(2);
+
+Insert into HumanHealth(health)
+Values(3);
+
+Insert into HumanHealth(health)
+Values(4);
+
+Insert into HumanHealth(health)
+Values(5);
+
+
+Insert into HumanGuardian(guardian)
+Values('mother');
+
+Insert into HumanGuardian(guardian)
+Values('father');
+
+Insert into HumanGuardian(guardian)
+Values('other');
+
+
+Insert into AlcoholConsDalc(dalc)
+Values(1);
+
+Insert into AlcoholConsDalc(dalc)
+Values(2);
+
+Insert into AlcoholConsDalc(dalc)
+Values(3);
+
+Insert into AlcoholConsDalc(dalc)
+Values(4);
+
+Insert into AlcoholConsDalc(dalc)
+Values(5);
+
+
+Insert into AlcoholConsWalc(walc)
+Values(1);
+
+Insert into AlcoholConsWalc(walc)
+Values(2);
+
+Insert into AlcoholConsWalc(walc)
+Values(3);
+
+Insert into AlcoholConsWalc(walc)
+Values(4);
+
+Insert into AlcoholConsWalc(walc)
+Values(5);
+
+
+Insert into SchoolTitle(title)
+Values('GP');
+
+Insert into SchoolTitle(title)
+Values('MS');
